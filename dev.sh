a608a6ef5a3da09c7944e85a5980a3266815766d
Denny Li <ytdli08@gmail.com>
2025-04-17 11:56:21 -0400

    added comments

--- dev.sh
+++ dev.sh
@@ -1,10 +1,16 @@
+# Remove the testing directories
 rm -rf test
 mkdir test
 mkdir test/{firefox,chrome}
+
+# Copy files over
 cp -r src/* test/firefox
 cp -r src/* test/chrome
+
+# Firefox
 mv test/firefox/manifest-firefox.json test/firefox/manifest.json
 rm test/firefox/manifest-chrome.json
 
+# Chrome
 mv test/chrome/manifest-chrome.json test/chrome/manifest.json
 rm test/chrome/manifest-firefox.json
