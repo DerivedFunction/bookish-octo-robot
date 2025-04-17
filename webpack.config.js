4384451a45265136bf6d71571943ca9f91bca51a
Denny Li <ytdli08@gmail.com>
2025-04-16 22:40:14 -0400

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const glob = require("glob");
+module.exports = [
+  {
+    entry: "./src/app.js",
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "app.js",
+    },
+  },
+  {
+    entry: "./src/background.js", // Service worker entry point
+    output: {
+      filename: "background.js", // Output bundle for service worker
+      path: path.resolve(__dirname, "dist"), // Output directory for service worker
+    },
+  },
+  {
+    entry: {
+      chatgpt: "./src/scripts/chatgpt.js",
+      gemini: "./src/scripts/gemini.js",
+      deepseek: "./src/scripts/deepseek.js",
+      grok: "./src/scripts/grok.js",
+      copilot: "./src/scripts/copilot.js",
+      claude: "./src/scripts/claude.js",
+      perplexity: "./src/scripts/perplexity.js",
+      mistral: "./src/scripts/mistral.js",
+      huggingface: "./src/scripts/hug.js",
+      meta: "./src/scripts/meta.js",
+    },
+    output: {
+      filename: "[name].js", // Output bundle for each script
+      path: path.resolve(__dirname, "dist/scripts"), // Output directory for all scripts
+    },
+  },
+];
