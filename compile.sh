3562a61f4204846d0dc73f6f30d7fd6573989def
Denny Li <ytdli08@gmail.com>
2025-04-16 22:38:32 -0400

    reconfigured workspace

--- compile.sh
+++ compile.sh
@@ -1,3 +1,20 @@
-rm -rf chrome/src
-cp -r src/ chrome/
-cp chrome/manifest.json chrome/src
+#!/bin/bash
+
+# Define source and destination directories
+SOURCE_DIR="src"
+DIST_DIR="dist"
+
+# Remove the existing distribution directory
+rm -rf "$DIST_DIR"
+
+# Create the distribution directory
+mkdir -p "$DIST_DIR"
+
+# Copy necessary files and directories to the distribution directory
+cp -r "$SOURCE_DIR"/*.{json,css,html} "$SOURCE_DIR"/{assets,scripts} "$DIST_DIR"
+
+# Run the npm build script
+npm run build
+
+cp -r dist/* firefox
+cp -r dist/* chrome
--- zip.sh
+++
@@ -1,6 +0,0 @@
-rm tabbed.zip
-cd src
-7z a ../tabbed.zip .
-cd ../chrome/src
-rm ../tabbed-chrome.zip
-7z a ../tabbed-chrome.zip .
