037a4c1386da9eebd3686997154475d631586b83
Denny Li <ytdli08@gmail.com>
2025-04-19 01:10:19 -0400

    updated toaster to include a title

--- src/modules/toaster.js
+++ src/modules/toaster.js
@@ -1,15 +1,18 @@
-export function showToast(message) {
+export function showToast(message, title = null) {
   const toasterContainer = document.getElementById("toaster-container");
   const toast = document.createElement("div");
   toast.classList.add("toast");
   const messageSpan = document.createElement("span");
+  const h1 = document.createElement("h1");
+  h1.textContent = title;
   messageSpan.classList.add("toast-message");
   messageSpan.textContent = message;
+  if (title) toast.appendChild(h1);
   toast.append(messageSpan);
   toasterContainer.appendChild(toast);
 
   // Remove the toast after a certain duration
   setTimeout(() => {
     toasterContainer.innerHTML = "";
-  }, 5000); // Adjust the duration as needed
+  }, 10000); // Adjust the duration as needed
 }
