c0889e3a92adf58f0fd1bf7ffe7252b2863dff8a
Denny Li <ytdli08@gmail.com>
2025-05-02 20:12:08 -0400

    removed unused import

--- src/modules/sidebar.js
+++ src/modules/sidebar.js
@@ -1,7 +1,7 @@
 import { weatherField } from "./weather.js";
 import { nameInput } from "./nameInput.js";
 import { appendSvg } from "./appendSvg.js";
-import { loadJsonData, toggleClass } from "../app.js";
+import { loadJsonData } from "../app.js";
 import {
   getPermissions,
   getScriptStatus,
